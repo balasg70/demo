58f0ba14376a4796a0e7e9cf2c3b26a930a2a756
balasg70 <56418977+balasg70@users.noreply.github.com>
2020-10-16 19:36:11 +0800

    Add files via upload

--- terraform/ec2-machines.tf
+++ terraform/ec2-machines.tf
@@ -4,8 +4,9 @@ resource "aws_instance" "phpapp" {
   associate_public_ip_address = "true"
   subnet_id = aws_subnet.PublicAZA.id
   vpc_security_group_ids = [aws_security_group.WebApp.id]
+  key_name = aws_key_pair.generated_key.key_name
   #key_name = var.key_name
-  key_name = "Testkey"
+  #key_name = "Testkey"
   tags = {
         Name = "ec2_devops"
   }
@@ -33,6 +34,7 @@ resource "tls_private_key" "example" {
 resource "aws_key_pair" "generated_key" {
   key_name   = var.key_name
   public_key = tls_private_key.example.public_key_openssh
+  #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwiYCPRY8IIo+PUwIuOBWXoksFiFVZHvhAiUP1p38mnU6BtWkYlWI19+xiPJzKAIiqL9jQZ2sm9qfT9Hx44IqmzLov/hxzkG/Jan358baO6L2XVkOlvK/YsKJ9x+gEO5N1JKM5mtA0vlUV/y5o13vmA8FiMYCA2+LnfG+JK40h7FhEcGWH/YUUh0P2PojoWSdaOH6uw1ZxjAGXYRHgaiio8INX3epxItL3dtVU4ZHPZdS8dPXpCUTNGnMLHt8VBXT4HhcQFf7BhgCo/nglqmig2JT/FgXk5fRzNfNG5piO3LAEBPdVfVTY/iKDpu0Dprm8GDEaZ12UJXzK3D58dXaR bala1@DESKTOP-7SQVHHL"
 }
 
 resource "aws_eip" "default" {
@@ -41,4 +43,9 @@ resource "aws_eip" "default" {
   instance                  = aws_instance.phpapp.id
   #associate_with_public_ip  = "10.0.0.12"
   depends_on                = [aws_internet_gateway.gw]
+}
+
+resource "local_file" "pem_file" { 
+  filename = "${path.module}/demotest.pem"
+  content = tls_private_key.example.private_key_pem
 }
